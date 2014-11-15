6a825de901d50b1a60d7e7f5ab7c40bcf22718b3
Oana Cocarascu <oana.cocarascu11@imperial.ac.uk>
2014-11-15 11:11:24 +0000

    Variance threshold decreasing order by variance.

--- variance_threshold.py
+++ variance_threshold.py
@@ -11,6 +11,9 @@ and can thus be used for unsupervised learning. '''
 print(__doc__)
 
 from utils.load_data import *
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import OrderedDict
 from sklearn.utils import validation
 from sklearn.feature_selection import VarianceThreshold
 
@@ -22,7 +25,17 @@ data = Data(spreadsheet)
 dataset = data.extract_illness_examples()
 features = data.illness_features
 
-model = VarianceThreshold(0.1)
+model = VarianceThreshold(0.2)
 a = model.fit_transform(dataset)
 
 
+# variance per feature
+feature_variance = np.var(dataset, axis = 0)
+assert len(feature_variance) == len(dataset[0])
+
+# create dictionary of feature and variance
+variance_per_feature = {}
+for i in range(0, len(feature_variance)):
+	variance_per_feature[features[i]] = feature_variance[i]
+
+print OrderedDict(sorted(variance_per_feature.items(), key=lambda t: t[1], reverse = True))
