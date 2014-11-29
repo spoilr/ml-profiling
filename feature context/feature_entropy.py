717b0060e38a1f2f1684bb0b09317e5bcdf2a182
Oana Cocarascu <oana.cocarascu11@imperial.ac.uk>
2014-11-29 16:58:04 +0000

    Print info gain for each attribute.

--- feature context/feature_entropy.py
+++ feature context/feature_entropy.py
@@ -49,32 +49,37 @@ def entropy(targets):
 
 # Information gain IG(A) is the measure of the difference in entropy from before to after the set S is split on an attribute A. 
 # ie How much uncertainty in S was reduced after splitting set S on attribute A.
-def info_gain(dataset, targets, attribute_col, entropy):
+def attribute_info_gain(dataset, targets, attribute_col, entropy):
 	length = len(targets)
 	values = set(attribute_col.tolist())
 
 	entropies = sum([prob_entropy_for_value(val, attribute_col, targets) for val in values])
 	return entropy - entropies
 
+def info_gain(dataset, features, targets, sys_entropy):
+	info_gains = []
+	nr_columns = dataset.shape[1]
+	for i in range(0, nr_columns):
+		attr_gain = attribute_info_gain(dataset, targets, dataset[:,i], sys_entropy)
+		info_gains.append(attr_gain)
+		print "%s info gain %f" % (features[i], attr_gain)
+	return info_gains	
+
+
 def prob_entropy_for_value(value, attribute_col, targets):
 	targets_for_value = [targets[i] for i in range(0, len(targets)) if attribute_col[i] == value]
 	return entropy(targets_for_value) * float(len(targets_for_value)) / len(targets)
 
+
 # tennis example
 def test_function():
+	features = ["out", "temp", "humid", "wind"]
 	targets = [0,0,1,1,1,0,1,0,1,1,1,1,1,0]
 	dataset = np.array([[1,1,1,1], [1,1,1,2], [2,1,1,1], [3,3,1,1], [3,2,2,1], [3,2,2,2,], [2,2,2,2], [1,3,1,1], [1,2,2,1], [3,3,2,1], [1,3,2,2], [2,3,1,2], [2,1,2,1], [3,3,1,2]])
 	assert len(dataset) == 14
 	sys_entropy = entropy(targets)
-	out_gain = info_gain(dataset, targets, dataset[:,0], sys_entropy)
-	temp_gain = info_gain(dataset, targets, dataset[:,1], sys_entropy)
-	hum_gain = info_gain(dataset, targets, dataset[:,2], sys_entropy)
-	wind_gain = info_gain(dataset, targets, dataset[:,3], sys_entropy)
-	print "entropy %f" % sys_entropy
-	print "out %f" % out_gain
-	print "temp %f" % temp_gain
-	print "hum %f" % hum_gain
-	print "wind %f" % wind_gain
+	print "entropy of system %f" % sys_entropy
+	info_gain(dataset, features, targets, sys_entropy)
 
 if __name__ == "__main__":
 	spreadsheet = Spreadsheet('/home/user/Downloads/ip/project data.xlsx')
@@ -83,7 +88,11 @@ if __name__ == "__main__":
 
 	[dataset, features] = parse_theme(sys.argv[1])
 	[known_dataset, known_targets, unk] = split_dataset(dataset, targets)
-	entropy(known_targets)
+
+	sys_entropy = entropy(known_targets)
+	print "entropy of system %f" % sys_entropy
+
+	info_gain(dataset, features, targets, sys_entropy)
 
 	# test_function()
 
