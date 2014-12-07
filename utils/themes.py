ac646e6191d9cf7e8abb6b280fc382baec5d7598
Oana Cocarascu <oana.cocarascu11@imperial.ac.uk>
2014-12-07 22:31:55 +0000

    Add all features that are not in theme to each theme.

--- utils/themes.py
+++ utils/themes.py
@@ -1,10 +1,44 @@
 #ideology = ['MilExp', 'Ideology', 'Religion', 'AwareGriev', 'AwareIdeo', 'IdeoChangeInt', 'ReligChangeInt', 'Legitimise', 'WideGroup', 'Tipping', 'LocationNature', 'LocPubPriv', 'Contradict', 'BeliefChange', 'Target', 'TargetGroup', 'RecruitNetGroup', 'Propaganda', 'OwnProp', 'PossessStories']
-ideology = ['MilExp', 'Ideology', 'Religion', 'AwareGriev', 'AwareIdeo', 'IdeoChangeInt', 'ReligChangeInt', 'Legitimise', 'WideGroup', 'Tipping', 'LocationNature', 'LocPubPriv', 'Contradict', 'BeliefChange', 'RecruitNetGroup', 'Propaganda', 'OwnProp', 'PossessStories']
+# ideology = ['MilExp', 'Ideology', 'Religion', 'AwareGriev', 'AwareIdeo', 'IdeoChangeInt', 'ReligChangeInt', 
+# 			'Legitimise', 'WideGroup', 'Tipping', 'LocationNature', 'LocPubPriv', 'Contradict', 'BeliefChange', 
+# 			'RecruitNetGroup', 'Propaganda', 'OwnProp', 'PossessStories']
 
-network = ['Funds', 'PersRelat', 'Involve', 'InteractNet', 'OtherInv', 'RecruitNetGroup']
+# network = ['Funds', 'PersRelat', 'Involve', 'InteractNet', 'OtherInv', 'RecruitNetGroup']
 
-illness = ['SubAbuse', 'MentalIll', 'Isolated', 'HarmVictimHelpless', 'HurtOthers', 'Stress', 'SubstanceUse', 'Obsess', 'Insanity']
+# illness = ['SubAbuse', 'MentalIll', 'Isolated', 'HarmVictimHelpless', 'HurtOthers', 'Stress', 'SubstanceUse', 'Obsess', 'Insanity']
 
-selected_features = ['RelatStat', 'Children', 'ParRelatStat', 'Education', 'OccCat', 'MilExp', 'CrimCon', 'Ideology', 'Religion', 'WarningLettersStatements', 'AwareGriev', 'AwareIdeo', 'IdeoChangeInt', 'ReligChangeInt', 'LifeAspectChange', 'Legitimise', 'Funds', 'SubAbuse', 'MentalIll', 'Isolated', 'DryRuns', 'WideGroup', 'NewMedia', 'Tipping', 'Interrupt', 'NotCareInjustice', 'HarmVictimHelpless', 'PersRelat', 'Financial', 'HurtOthers', 'Stress', 'SubstanceUse', 'Violence', 'TargetTyp', 'LocationNature', 'LocPubPriv', 'Stockpile', 'Contradict', 'Obsess', 'Regret', 'BeliefChange', 'Insanity', 'Implement', 'MultiAttackMeth', 'Discriminate', 'LettersPost', 'Getaway', 'MultiEventTarget', 'Victims', 'Involve', 'InteractNet', 'OtherInv', 'OtherKnowledge', 'RecruitNetGroup', 'Propaganda', 'OwnProp', 'LAKilled', 'FurtherAttacks', 'ClaimResp', 'PossessStories']
+selected_features = ['RelatStat', 'Children', 'ParRelatStat', 'Education', 'OccCat', 'MilExp', 'CrimCon', 'Ideology', 'Religion', 
+					'WarningLettersStatements', 'AwareGriev', 'AwareIdeo', 'IdeoChangeInt', 'ReligChangeInt', 'LifeAspectChange', 
+					'Legitimise', 'Funds', 'SubAbuse', 'MentalIll', 'Isolated', 'DryRuns', 'WideGroup', 'NewMedia', 'Tipping', 
+					'Interrupt', 'NotCareInjustice', 'HarmVictimHelpless', 'PersRelat', 'Financial', 'HurtOthers', 'Stress', 
+					'SubstanceUse', 'Violence', 'TargetTyp', 'LocationNature', 'LocPubPriv', 'Stockpile', 'Contradict', 
+					'Obsess', 'Regret', 'BeliefChange', 'Insanity', 'Implement', 'MultiAttackMeth', 'Discriminate', 'LettersPost', 
+					'Getaway', 'MultiEventTarget', 'Victims', 'Involve', 'InteractNet', 'OtherInv', 'OtherKnowledge', 
+					'RecruitNetGroup', 'Propaganda', 'OwnProp', 'LAKilled', 'FurtherAttacks', 'ClaimResp', 'PossessStories']
 
+ideology = ['RelatStat', 'Children', 'ParRelatStat', 'Education', 'OccCat', 'MilExp', 'CrimCon', 'Ideology', 'Religion', 
+			'WarningLettersStatements', 'AwareGriev', 'AwareIdeo', 'IdeoChangeInt', 'ReligChangeInt', 'LifeAspectChange', 
+			'Legitimise', 'DryRuns', 'WideGroup', 'NewMedia', 'Tipping', 
+			'Interrupt', 'NotCareInjustice', 'Financial', 
+			'Violence', 'TargetTyp', 'LocationNature', 'LocPubPriv', 'Stockpile', 'Contradict', 
+			'Regret', 'BeliefChange', 'Implement', 'MultiAttackMeth', 'Discriminate', 'LettersPost', 
+			'Getaway', 'MultiEventTarget', 'Victims', 'OtherKnowledge', 
+			'Propaganda', 'OwnProp', 'LAKilled', 'FurtherAttacks', 'ClaimResp', 'PossessStories']
 
+network = ['RelatStat', 'Children', 'ParRelatStat', 'Education', 'OccCat', 'CrimCon', 
+			'WarningLettersStatements', 'LifeAspectChange', 
+			'Funds', 'DryRuns', 'NewMedia',  
+			'Interrupt', 'NotCareInjustice', 'PersRelat', 'Financial', 
+			'Violence', 'TargetTyp', 'Stockpile', 
+			'Regret', 'Implement', 'MultiAttackMeth', 'Discriminate', 'LettersPost', 
+			'Getaway', 'MultiEventTarget', 'Victims', 'Involve', 'InteractNet', 'OtherInv', 'OtherKnowledge', 
+			'LAKilled', 'FurtherAttacks', 'ClaimResp']
+
+illness = ['RelatStat', 'Children', 'ParRelatStat', 'Education', 'OccCat', 'CrimCon', 
+			'WarningLettersStatements', 'LifeAspectChange', 
+			'SubAbuse', 'MentalIll', 'Isolated', 'DryRuns', 'NewMedia',  
+			'Interrupt', 'NotCareInjustice', 'HarmVictimHelpless', 'Financial', 'HurtOthers', 'Stress', 
+			'SubstanceUse', 'Violence', 'TargetTyp', 'Stockpile', 
+			'Obsess', 'Regret', 'Insanity', 'Implement', 'MultiAttackMeth', 'Discriminate', 'LettersPost', 
+			'Getaway', 'MultiEventTarget', 'Victims', 'OtherKnowledge', 
+			'LAKilled', 'FurtherAttacks', 'ClaimResp']
