a673171dabc388381376bc3c04bfd516ca578b7e
Oana Cocarascu <oana@Oanas-Air.home>
2015-01-24 20:17:37 +0000

    File path.

--- utils/project_data.py
+++ utils/project_data.py
@@ -1,1 +1,2 @@
-project_data_file = '/home/user/Downloads/ip/project data.xlsx'
+# project_data_file = '/home/user/Downloads/ip/project data.xlsx'
+project_data_file = '/Users/oana/Downloads/ip/project data no.xlsx'
