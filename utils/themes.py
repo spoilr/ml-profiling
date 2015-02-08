0af4325ce111ae6f89bf57bf3c5001a495e01181
Oana Cocarascu <oana@Oanas-Air.home>
2015-02-08 22:59:04 +0000

    Added 6 vars.

--- utils/themes.py
+++ utils/themes.py
@@ -6,9 +6,11 @@
 
 # illness = ['SubAbuse', 'MentalIll', 'Isolated', 'AwareGriev', 'Tipping', 'HarmVictimHelpless', 'HurtOthers', 'Stress', 'SubstanceUse', 'Obsess', 'Insanity']
 
+
 subset_features = ['RelatStat', 'Children', 'ParRelatStat', 'Education', 'OccCat', 'MilExp', 'CrimCon', 'Ideology', 'Religion', 
 					'WarningLettersStatements', 'AwareGriev', 'AwareIdeo', 'IdeoChangeInt', 'ReligChangeInt', 'LifeAspectChange', 
-					'Legitimise', 'Funds', 'SubAbuse', 'MentalIll', 'Isolated', 'DryRuns', 'WideGroup', 'NewMedia', 'Tipping', 
+					'Legitimise', 'Funds', 'Denounce', 'LiveAlone', 'Adoption', 'Training', 'Virtual', 
+					'SubAbuse', 'MentalIll', 'Isolated', 'DryRuns', 'BombManuals', 'WideGroup', 'NewMedia', 'Tipping', 
 					'Interrupt', 'NotCareInjustice', 'HarmVictimHelpless', 'PersRelat', 'Financial', 'HurtOthers', 'Stress', 
 					'SubstanceUse', 'Violence', 'TargetTyp', 'LocationNature', 'LocPubPriv', 'Stockpile', 'Contradict', 
 					'Obsess', 'Regret', 'BeliefChange', 'Insanity', 'Implement', 'MultiAttackMeth', 'Discriminate', 'LettersPost', 
@@ -17,7 +19,7 @@ subset_features = ['RelatStat', 'Children', 'ParRelatStat', 'Education', 'OccCat
 
 ideology = ['RelatStat', 'Children', 'ParRelatStat', 'Education', 'OccCat', 'MilExp', 'CrimCon', 'Ideology', 'Religion', 
 			'WarningLettersStatements', 'AwareIdeo', 'IdeoChangeInt', 'ReligChangeInt', 'LifeAspectChange', 
-			'Legitimise', 'DryRuns', 'WideGroup', 'NewMedia',
+			'Legitimise', 'Denounce', 'LiveAlone', 'Adoption', 'Training', 'Virtual', 'DryRuns', 'BombManuals', 'WideGroup', 'NewMedia',
 			'Interrupt', 'NotCareInjustice', 'Financial', 
 			'Violence', 'TargetTyp', 'LocationNature', 'LocPubPriv', 'Stockpile', 'Contradict', 
 			'Regret', 'BeliefChange', 'Implement', 'MultiAttackMeth', 'Discriminate', 'LettersPost', 
@@ -26,16 +28,16 @@ ideology = ['RelatStat', 'Children', 'ParRelatStat', 'Education', 'OccCat', 'Mil
 
 network = ['RelatStat', 'Children', 'ParRelatStat', 'Education', 'OccCat', 'CrimCon', 
 			'WarningLettersStatements', 'LifeAspectChange', 
-			'Funds', 'DryRuns', 'NewMedia',  
+			'Funds', 'LiveAlone', 'Virtual', 'DryRuns', 'BombManuals', 'NewMedia',  
 			'Interrupt', 'NotCareInjustice', 'PersRelat', 'Financial', 
 			'Violence', 'TargetTyp', 'Stockpile', 
 			'Regret', 'Implement', 'MultiAttackMeth', 'Discriminate', 'LettersPost', 
 			'Getaway', 'MultiEventTarget', 'Involve', 'InteractNet', 'OtherInv', 'OtherKnowledge', 
-			'LAKilled', 'FurtherAttacks', 'ClaimResp']
+			'RecruitNetGroup', 'LAKilled', 'FurtherAttacks', 'ClaimResp']
 
 illness = ['RelatStat', 'Children', 'ParRelatStat', 'Education', 'OccCat', 'CrimCon', 
 			'WarningLettersStatements', 'AwareGriev', 'LifeAspectChange', 
-			'SubAbuse', 'MentalIll', 'Isolated', 'DryRuns', 'NewMedia', 'Tipping',  
+			'SubAbuse', 'MentalIll', 'Isolated', 'LiveAlone', 'Virtual', 'DryRuns', 'BombManuals', 'NewMedia', 'Tipping',  
 			'Interrupt', 'NotCareInjustice', 'HarmVictimHelpless', 'Financial', 'HurtOthers', 'Stress', 
 			'SubstanceUse', 'Violence', 'TargetTyp', 'Stockpile', 
 			'Obsess', 'Regret', 'Insanity', 'Implement', 'MultiAttackMeth', 'Discriminate', 'LettersPost', 
