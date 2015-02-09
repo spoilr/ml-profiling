c73a9080404ea2be7c995136e9b469b5f6b92887
Oana Cocarascu <oana@dyn1224-121.wlan.ic.ac.uk>
2015-02-09 13:41:09 +0000

    SVM with best params. (they all give the same result)

--- classification/svms.py
+++ classification/svms.py
@@ -1,25 +1,25 @@
 from sklearn.svm import SVC
 
 def svm_all_vars(dataset, targets):
-	model = SVC(class_weight='auto')
+	model = SVC(class_weight='auto', C=1.20001, gamma=0.30001)
 	model.fit(dataset, targets)
 	# print 'Model score: %f' % model.score(known_dataset, known_targets)
 	return model
 
 def svm_selected_vars(dataset, targets):
-	model = SVC(class_weight='auto')
+	model = SVC(class_weight='auto', C=1.20001, gamma=0.30001)
 	model.fit(dataset, targets)
 	# print 'Model score: %f' % model.score(known_dataset, known_targets)
 	return model
 
 def svm_for_features_fusion(dataset, targets):
-	model = SVC(class_weight='auto')
+	model = SVC(class_weight='auto', C=0.90000999999999987, gamma=0.30001)
 	model.fit(dataset, targets)
 	# print 'Model score: %f' % model.score(known_dataset, known_targets)
 	return model
 
 def svm_subset_features(dataset, targets):
-	model = SVC(class_weight='auto')
+	model = SVC(class_weight='auto', C=0.90000999999999987, gamma=0.30001)
 	model.fit(dataset, targets)
 	# print 'Model score: %f' % model.score(known_dataset, known_targets)
 	return model
