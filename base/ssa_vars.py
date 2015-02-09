00da334111608d32ac3f18d8adbd4f64c72d71f2
Oana Cocarascu <oana@dyn1224-121.wlan.ic.ac.uk>
2015-02-09 12:44:21 +0000

    Vars used in report based on the new features that I created.

---
+++ base/ssa_vars.py
@@ -0,0 +1,17 @@
+vars_45 = ['JuvArrest', 'SubAbuse', 'MilExp', 'Warning', 'Propaganda', 'PhysEd', 'PropLA', 'Convert', 'Legit', 'Adopt', 'WiderGroup',
+			'Debts', 'Religiosity', 'CrimC', 'DryRuns', 'DropOut', 'LiveAlone', 'VirtualL', 'Virtuali', 'PropG', 'AddC', 'Univ',
+			'SpouseI', 'Denounce', 'OtherP', 'OtherK', 'BombM', 'Train', 'OtherB', 'CandC', 'Prison', 'CloseA', 'F2F',
+			'AwareI', 'AwareG', 'Letter', 'VerbF', 'Recruit', 'VerbS', 'Isolated', 'Unemp', 'BankAcc', 'Mental', 'Funds', 'Reject']
+
+# Convert and Religiosity are combined in one variable Religion
+# PhysicalEducation removed
+# Address change removed
+
+ssa_vars = ['SubAbuse', 'MilExp', 'Propaganda', 'Religion', 'Legitimise', 'Adoption', 'WideGroup', 'CrimCon', 
+			'DryRuns', 'LiveAlone', 'Virtual', 'Involve', 'Denounce', 'OtherInv', 'OtherKnowledge', 
+			'BombManuals', 'Training', 'RecruitNetGroup', 'InteractNet', 'AwareIdeo', 'AwareGriev', 
+			'WarningLettersStatements', 'Isolated', 'LifeAspectChange', 'MentalIll', 'Funds']
+
+
+
+
