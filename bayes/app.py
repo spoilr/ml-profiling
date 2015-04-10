e062c5758feed8412a4330cafff9b63904e49bd1
Oana Cocarascu <oana.cocarascu11@imperial.ac.uk>
2015-04-10 18:50:33 +0300

    Html pages for bayes. Display evidence encodings. No submit.

--- bayes/app.py
+++ bayes/app.py
@@ -2,39 +2,79 @@
 from flask import Flask
 from flask import jsonify
 from flask import request
+from flask import render_template
+from flask import url_for
 from create_struct import *
+
+import sys
+sys.path.insert(0, 'utils/')
+
+from explanatory_data import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return "Hello World"
+	
+@app.route('/bayes/net')
+def bayes_net():
+	nodes = bn_net.V
+	categs = dict()
+	for n in nodes:
+		if n in explanatory_data:
+			categs[n] = explanatory_data[n]
+		else:
+			categs[n] = {0:"no", 1:"yes"}
+	print categs		
+	return render_template('net.html', categs=categs)	
+
+@app.route('/bayes/ill')
+def bayes_ill():
+	nodes = bn_ill.V
+	categs = dict()
+	for n in nodes:
+		if n in explanatory_data:
+			categs[n] = explanatory_data[n]
+		else:
+			categs[n] = {0:"no", 1:"yes"}
+	print categs		
+	return render_template('net.html', categs=categs)	
+
+@app.route('/bayes/ideo')
+def bayes_ideo():
+	nodes = bn_ideo.V
+	categs = dict()
+	for n in nodes:
+		if n in explanatory_data:
+			categs[n] = explanatory_data[n]
+		else:
+			categs[n] = {0:"no", 1:"yes"}
+	print categs		
+	return render_template('net.html', categs=categs)
 
 @app.route('/net', methods=['GET'])
 def net_inferences():
 	evidence = request.args.to_dict()
 	evidence = dict((k,int(v)) for k,v in evidence.iteritems())
-	bn = create_bayesian_network_structure('net')
-	fn = TableCPDFactorization(bn)
-	inf = inference(bn, evidence)
+	fn = TableCPDFactorization(bn_net)
+	inf = inference(bn_net, evidence)
 	return inf
 
 @app.route('/ill', methods=['GET'])
 def ill_inferences():
 	evidence = request.args.to_dict()
 	evidence = dict((k,int(v)) for k,v in evidence.iteritems())
-	bn = create_bayesian_network_structure('ill')
-	fn = TableCPDFactorization(bn)
-	inf = inference(bn, evidence)
+	fn = TableCPDFactorization(bn_ill)
+	inf = inference(bn_ill, evidence)
 	return inf
 
 @app.route('/ideo', methods=['GET'])
 def ideo_inferences():
 	evidence = request.args.to_dict()
 	evidence = dict((k,int(v)) for k,v in evidence.iteritems())
-	bn = create_bayesian_network_structure('ideo')
-	fn = TableCPDFactorization(bn)
-	inf = inference(bn, evidence)
+	fn = TableCPDFactorization(bn_ideo)
+	inf = inference(bn_ideo, evidence)
 	return inf	
 
 def shutdown_server():
@@ -49,4 +89,7 @@ def shutdown():
     return 'Server shutting down...'
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	bn_net = create_bayesian_network_structure('net')
+	bn_ill = create_bayesian_network_structure('ill')
+	bn_ideo = create_bayesian_network_structure('ideo')
+	app.run(debug=True, threaded=True)
