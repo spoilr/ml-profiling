bda6befa2525ded3f64c82bda78558764204d5bc
Oana Cocarascu <oana.cocarascu11@imperial.ac.uk>
2015-05-05 21:14:54 +0100

    Join tree inference.

--- bayes/join_tree.py
+++ bayes/join_tree.py
@@ -17,6 +17,8 @@ def create_cpts_from_parents(values, parents, cprob):
 
 def create_bbn_network(Vdata):
 	cpts = dict()
+	key_dict = dict()
+
 	for x in Vdata.iteritems():
 		key = x[0]
 		parents = x[1]['parents']
@@ -24,26 +26,31 @@ def create_bbn_network(Vdata):
 		values = x[1]['vals']
 		cprob = x[1]['cprob']
 		values = map(str, values)
+		
+		key_dict[key] = {}
+		if not parents and not children:
+			key_dict[key] = dict(zip(values, cprob))
 
 		if parents:
 			cpts[str(key)] = create_cpts_from_parents(values, parents, cprob)
 		elif children:
 			cpts[str(key)] = [[[], dict(zip(values, cprob))]]
 
-	node_funcs = []
-	domains = dict()
-	for variable_name, cond_tt in cpts.items():
-		node_func = make_node_func(variable_name, cond_tt)
-		node_funcs.append(node_func)
-		domains[variable_name] = node_func._domain
-
-	return build_bbn_from_conditionals(cpts)
+	return build_bbn_from_conditionals(cpts), key_dict
 	
+def inference(network):
+	bn, inf = create_bbn_network(network.Vdata)		
+	result = bn.query()
+	for (key, val), prob in result.iteritems():
+		inf[key][val] = prob
+	return json.dumps(inf, indent=2)
+
 if __name__ == "__main__":
 	bn_net = create_bayesian_network_structure('ideo')
-	bn = create_bbn_network(bn_net.Vdata)		
+	bn, key_dict = create_bbn_network(bn_net.Vdata)		
 	result = bn.query()
 	print result
+	print inference(bn_net)
 
 
 
