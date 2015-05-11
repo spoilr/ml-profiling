b976b7c385cd34ed58179ce2f0563e676bc4afe3
Oana Cocarascu <oana.cocarascu11@imperial.ac.uk>
2015-05-11 21:50:54 +0100

    All files for target checks.

--- utils/project_data.py
+++ utils/project_data.py
@@ -1,3 +1,11 @@
 # project_data_file = '/home/user/Downloads/ip/project data.xlsx'
 project_data_file = '/Users/oana/Downloads/ip/project data no.xlsx'
 addendum_data_file = '/Users/oana/Downloads/ip/addendum.xlsx'
+
+d_project_data_file = '/Users/oana/Downloads/ip/project data no d.xlsx'
+g_project_data_file = '/Users/oana/Downloads/ip/project data no g.xlsx'
+v_project_data_file = '/Users/oana/Downloads/ip/project data no v.xlsx'
+
+d_addendum_data_file = '/Users/oana/Downloads/ip/addendum d.xlsx'
+g_addendum_data_file = '/Users/oana/Downloads/ip/addendum g.xlsx'
+v_addendum_data_file = '/Users/oana/Downloads/ip/addendum v.xlsx'
