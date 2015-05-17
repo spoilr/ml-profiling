7ea82b8807bf7cb9da6bf38f65815d2afe74f90e
Oana Cocarascu <oana.cocarascu11@imperial.ac.uk>
2015-05-17 01:50:02 +0100

    SPSS files from similarity matrices and similairty matrices using Jaccard.

--- utils/project_data.py
+++ utils/project_data.py
@@ -9,3 +9,13 @@ v_project_data_file = '/Users/oana/Downloads/ip/project data no v.xlsx'
 d_addendum_data_file = '/Users/oana/Downloads/ip/addendum d.xlsx'
 g_addendum_data_file = '/Users/oana/Downloads/ip/addendum g.xlsx'
 v_addendum_data_file = '/Users/oana/Downloads/ip/addendum v.xlsx'
+
+highval_binary_all_data = '/Users/oana/Downloads/ip/highval_binary_all_data.xlsx'
+highval_binary_ideo_data = '/Users/oana/Downloads/ip/highval_binary_ideo_data.xlsx'
+highval_binary_ill_data = '/Users/oana/Downloads/ip/highval_binary_ill_data.xlsx'
+highval_binary_net_data = '/Users/oana/Downloads/ip/highval_binary_net_data.xlsx'
+
+civil_binary_all_data = '/Users/oana/Downloads/ip/civil_binary_all_data.xlsx'
+civil_binary_ideo_data = '/Users/oana/Downloads/ip/civil_binary_ideo_data.xlsx'
+civil_binary_ill_data = '/Users/oana/Downloads/ip/civil_binary_ill_data.xlsx'
+civil_binary_net_data = '/Users/oana/Downloads/ip/civil_binary_net_data.xlsx'
