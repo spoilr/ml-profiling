d22f75caa4fb1c094f1447b6ab86ba2916495b9b
Oana Cocarascu <oana.cocarascu11@imperial.ac.uk>
2015-05-18 01:11:31 +0100

    Evidence for Bayes.

--- test/bayes_inference_test.py
+++ test/bayes_inference_test.py
@@ -6,10 +6,36 @@ from load_data import *
 from parse_theme import *
 from project_data import *
 
-def inference_accuracy(dataset):
+import itertools
+
+net_evidence = ['InteractNet', 'RecruitNetGroup', 'OtherKnowledge', 'Involve', 'LiveAlone', 'WarningLettersStatements', 'TargetTyp', 'HighValueCivilian', 'FurtherAttacks']
+
+ill_evidence = ['AwareGriev', 'OtherKnowledge', 'Isolated', 'FurtherAttacks', 'WarningLettersStatements', 'LiveAlone', 'Stress', 'HighValueCivilian', 'Getaway', 'MentalIll']
+
+ideo_evidence = ['ReligChangeInt', 'HighValueCivilian', 'RecruitNetGroup', 'Ideology', 'LiveAlone', 'TargetTyp', 'Propaganda', 'Legitimise', 'LocPubPriv', 'OtherKnowledge', 'IdeoChangeInt', 'AwareIdeo', 'Religion', 'FurtherAttacks']
+
+
+def likelihood_from_inference(inference):
+	inf = dict()
+	inference = json.loads(inference)
+	for key, vals in inference.items():
+		inf[key] = max(vals.items(), key=lambda x: x[1])
+	return inf	
+
+def evidence_from_theme(theme):
+	if theme == 'net':
+		return net_evidence
+	elif theme == 'ill':
+		return ill_evidence
+	elif theme == 'ideo':
+		return ideo_evidence
+	else:
+		print 'ERROR'	
+
+def inference_accuracy(dataset, nodes, features, inf):
 	accuracy = 0
 	for instance in dataset:
-		accuracy += inference_accuracy_for_instance(bn.V, features, inf, instance)
+		accuracy += inference_accuracy_for_instance(nodes, features, inf, instance)
 	return float(accuracy) / len(dataset)	
 
 def inference_accuracy_for_instance(nodes, features, inference, instance):
@@ -20,15 +46,38 @@ def inference_accuracy_for_instance(nodes, features, inference, instance):
 			nrs+=1
 
 	accuracy = float(nrs)/len(nodes)
-	print 'Accuracy ' + str(accuracy)
+	# print 'Accuracy ' + str(accuracy)
 	return accuracy
 
-def likelihood_from_inference(inference):
-	inf = dict()
-	inference = json.loads(inference)
-	for key, vals in inference.items():
-		inf[key] = max(vals.items(), key=lambda x: x[1])
-	return inf	
+def create_combinations_evidence(possible_evidence):
+	combinations_possible_evidence = []
+
+	# single elements
+	for x in possible_evidence:
+		combinations_possible_evidence.append([x])
+
+	for i in xrange(2,len(possible_evidence)+1):
+		combinations = list(itertools.combinations(possible_evidence,i))
+		combinations_possible_evidence = combinations_possible_evidence + (map(list,combinations))
+
+	return combinations_possible_evidence		
+
+def create_evidence_from_list(x):
+	ev = dict()
+	for elem in x:
+		ev[elem] = 1
+	return ev
+
+def propagate_evidence(bn, possible_evidence, features):
+	combinations_possible_evidence = create_combinations_evidence(possible_evidence)
+	for x in combinations_possible_evidence:
+		evidence = create_evidence_from_list(x)
+		inf = inference(bn, evidence)
+		inf = likelihood_from_inference(inf)
+		# print inf
+
+		accuracy = inference_accuracy(dataset, bn.V, features, inf)
+		print accuracy
 
 if __name__ == "__main__":
 	theme = raw_input("Enter theme.\n")
@@ -44,11 +93,10 @@ if __name__ == "__main__":
 	features.append('HighValueCivilian')	# append target name in feature
 	assert dataset.shape[0] == len(targets)
 
-	evidence = dict(InteractNet=0)
-	inf = inference(bn, evidence)
-	inf = likelihood_from_inference(inf)
-	# print inf
+	# evidence = dict(InteractNet=1)
+	# evidence = dict(MentalIll=1)
+	# evidence = dict(Religion=1, Ideology=4)
 
-	accuracy = inference_accuracy(dataset)
-	print accuracy
+	possible_evidence = evidence_from_theme(theme)
+	propagate_evidence(bn, possible_evidence, features)
 	
